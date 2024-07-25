208bce9c8c6e0170c99aec5a0c0d907630466a6e
Deepak Kumar <mahtodeepak857@gmail.com>
2024-07-25 12:01:20 +0530

    evenorodd

--- assignments-java/05-methods/methods/src/EvenOrOdd.java
+++ assignments-java/05-methods/methods/src/EvenOrOdd.java
@@ -1,3 +1,20 @@
-public class EvenOrOdd {
+import java.util.Scanner;
 
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = in.nextInt();
+        String result = evenOrOdd(num);
+        System.out.println("Result: "+ result);
+    }
+    static String evenOrOdd(int n) {
+        String result = "";
+        if (n % 2 == 0) {
+            result += "even";
+        } else {
+            result += "odd";
+        }
+        return result;
+    }
 }
