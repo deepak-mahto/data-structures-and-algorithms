c92a6ce609f8729ce6530b3ffd374d2107d1c425
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-28 08:22:36 +0530

    Renaming files

--- Java/04-functions(methods)/functions/src/Greeting2.java
+++
@@ -1,9 +0,0 @@
-public class Greeting2 {
-    public static void main(String[] args) {
-        String message = greet();
-        System.out.println(message);
-    }
-    static String greet() {
-        return "hey! how are you?";
-    }
-}
